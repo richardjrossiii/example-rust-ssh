a779410387c4d24606d02cd542b8e6c22d1b3d4d
Richard Ross <richardross@meta.com>
2026-04-16 14:57:34 -0700

    initial commit

---
+++ process-commands/src/main.rs
@@ -0,0 +1,33 @@
+use serde::Deserialize;
+use std::fs;
+use ssh2::Session;
+use anyhow::Result;
+use std::net::TcpStream;
+use std::io::Read;
+
+#[derive(Deserialize)]
+struct Host {
+    name: String,
+    port: u16,
+    command: String,
+}
+
+fn main() -> Result<()> {
+    let mut output = Vec::new();
+    let input = std::fs::read_to_string("input/hosts.json")?;
+    let commands_to_process: Vec<Host> = serde_json::from_str(&input)?;
+
+    for command in commands_to_process {
+        let tcp = TcpStream::connect(format!("{}:{}", command.name, command.port)).unwrap();
+
+        let mut sess = Session::new().unwrap();
+        sess.set_tcp_stream(tcp);
+        sess.handshake().unwrap();
+
+        let mut channel = sess.channel_session().unwrap();
+        channel.exec(&command.command).unwrap();
+        channel.read_to_end(&mut output).unwrap();
+    }
+
+    Ok(())
+}
