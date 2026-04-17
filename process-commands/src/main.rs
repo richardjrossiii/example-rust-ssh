0fdfbd1738fd2eb69e3660b14afc60e43153cc07
Richard Ross <richardross@meta.com>
2026-04-16 17:14:35 -0700

    minor refactor

--- process-commands/src/main.rs
+++ process-commands/src/main.rs
@@ -4,6 +4,7 @@ use anyhow::Result;
 use std::net::TcpStream;
 use std::io::Read;
 use std::path::Path;
+use anyhow::Context;
 
 #[derive(Deserialize)]
 struct Host {
@@ -12,34 +13,41 @@ struct Host {
     command: String,
 }
 
+fn run_command(command: Host) -> Result<String> {
+    let tcp = TcpStream::connect((command.name, command.port)).context("failed to connect")?;
+
+    let mut output = Vec::new();
+    let mut sess = Session::new().context("failed to create session")?;
+    sess.set_tcp_stream(tcp);
+    sess.handshake().context("failed to perform handshake")?;
+    sess.userauth_pubkey_file(
+        "root", 
+        Some(Path::new("/root/.ssh/id_rsa.pub")), 
+        Path::new("/root/.ssh/id_rsa"),
+        None,
+    ).unwrap();
+
+    let mut channel = sess.channel_session().context("Failed to create channel")?;
+    channel.exec(&command.command).context("Failed to exec")?;
+    channel.read_to_end(&mut output).context("Failed to read output")?;
+
+    String::from_utf8(output).context("invalid utf-8")
+}
+
 fn main() -> Result<()> {
     let process_args: Vec<_> = std::env::args().collect();
-    let mut output = Vec::new();
+    let mut output = String::new(); 
     
     let input = std::fs::read_to_string(&process_args[1])?;
     let commands_to_process: Vec<Host> = serde_json::from_str(&input)?;
 
     for command in commands_to_process {
-        let tcp = TcpStream::connect((command.name, command.port)).unwrap();
+        let result = run_command(command)?;
 
-        let mut sess = Session::new().unwrap();
-        sess.set_tcp_stream(tcp);
-        sess.handshake().unwrap();
-        sess.userauth_pubkey_file(
-            "root", 
-            Some(Path::new("/root/.ssh/id_rsa.pub")), 
-            Path::new("/root/.ssh/id_rsa"),
-            None,
-        ).unwrap();
-
-        let mut channel = sess.channel_session().unwrap();
-        channel.exec(&command.command).unwrap();
-        channel.read_to_end(&mut output).unwrap();
-
-        output.push(b'\n');
+        output += &result; 
     }
 
-    println!("{}", String::from_utf8_lossy(&output));
+    println!("{}", output);
 
     Ok(())
 }
