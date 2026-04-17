e8c71794b0214b1110355375fb9d09f4c81cfd81
Richard Ross <richardross@meta.com>
2026-04-16 17:58:03 -0700

    async solution

--- process-commands/src/main.rs
+++ process-commands/src/main.rs
@@ -1,10 +1,10 @@
 use anyhow::Context;
 use anyhow::Result;
+use async_ssh2_tokio::client::{AuthMethod, Client, ServerCheckMethod};
+use futures::stream;
+use futures::stream::StreamExt;
+use itertools::Itertools;
 use serde::Deserialize;
-use ssh2::Session;
-use std::io::Read;
-use std::net::TcpStream;
-use std::path::Path;
 
 #[derive(Deserialize)]
 struct Host {
@@ -13,44 +13,55 @@ struct Host {
     command: String,
 }
 
-fn run_command(command: Host) -> Result<String> {
-    let tcp = TcpStream::connect((command.name, command.port)).context("failed to connect")?;
-
-    let mut output = Vec::new();
-    let mut sess = Session::new().context("failed to create session")?;
-    sess.set_tcp_stream(tcp);
-    sess.handshake().context("failed to perform handshake")?;
-    sess.userauth_pubkey_file(
+async fn run_command(command: &Host) -> Result<String> {
+    let auth_method = AuthMethod::with_key_file("/root/.ssh/id_rsa", None);
+    let client = Client::connect(
+        (command.name.as_str(), command.port),
         "root",
-        Some(Path::new("/root/.ssh/id_rsa.pub")),
-        Path::new("/root/.ssh/id_rsa"),
-        None,
+        auth_method,
+        ServerCheckMethod::NoCheck,
     )
-    .unwrap();
+    .await
+    .context("failed to connect")?;
 
-    let mut channel = sess.channel_session().context("Failed to create channel")?;
-    channel.exec(&command.command).context("Failed to exec")?;
-    channel
-        .read_to_end(&mut output)
-        .context("Failed to read output")?;
+    let result = client
+        .execute(&command.command)
+        .await
+        .context("failed to execute command")?;
 
-    String::from_utf8(output).context("invalid utf-8")
+    Ok(result.stdout.to_string())
 }
 
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     let process_args: Vec<_> = std::env::args().collect();
-    let mut output = String::new();
 
     let input = std::fs::read_to_string(&process_args[1])?;
     let commands_to_process: Vec<Host> = serde_json::from_str(&input)?;
 
-    for command in commands_to_process {
-        let result = run_command(command)?;
+    let ouptuts = stream::iter(
+        commands_to_process
+            .iter()
+            .chunk_by(|command| &command.name)
+            .into_iter(),
+    )
+    .map(async |(_, commands)| {
+        let mut output = String::new();
+        for command in commands {
+            let result = run_command(command).await?;
 
-        output += &result;
-    }
+            output += &result;
+        }
 
-    println!("{}", output);
+        Ok(output)
+    })
+    .buffer_unordered(128)
+    .collect::<Vec<_>>()
+    .await
+    .into_iter()
+    .collect::<Result<Vec<_>>>()?;
+
+    println!("{}", ouptuts.join("\n"));
 
     Ok(())
 }
